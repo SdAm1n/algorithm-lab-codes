25299edd98ee28edafb4e6106c57f996b5e3a10c
Md. Sadikul Amin Sadman <anarchst49@gmail.com>
2024-05-21 04:30:17 +0600

    added maximum subarray sum problem

---
+++ Greedy/maximum_subarray_sum.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i = 0; i < n; i++) cin >> arr[i];
+
+    int best = 0, sum = 0;
+    for (int i = 0; i < n; i++) {
+        sum = max (arr[i], sum + arr[i]);
+        best = max(sum, best);
+    }
+    cout << best << endl;
+}
